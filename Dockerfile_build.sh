ceed859a71fd8d489e8933cbba87ad931a25d024
shimwell <mail@jshimwell.com>
2019-07-13 09:34:51 +0100

    revesed flags

--- Dockerfile_build.sh
+++ Dockerfile_build.sh
@@ -1,2 +1,2 @@
-sudo docker build -t shimwell:nuclear_xs_viewer_build -f Docerfile_build 
+sudo docker build -f Docerfile_build -t shimwell:nuclear_xs_viewer_build 
 # sudo docker run -v $PWD:/app/build
