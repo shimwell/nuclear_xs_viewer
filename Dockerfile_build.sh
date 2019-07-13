ac58aff09ecd5c44dc6ae125afd3ea61da34d0a0
shimwell <mail@jshimwell.com>
2019-07-13 09:38:17 +0100

    added .

--- Dockerfile_build.sh
+++ Dockerfile_build.sh
@@ -1,2 +1,2 @@
-sudo docker build -f Docerfile_build -t shimwell:nuclear_xs_viewer_build 
+sudo docker build -f Dockerfile_build -t shimwell:nuclear_xs_viewer_build .
 # sudo docker run -v $PWD:/app/build
