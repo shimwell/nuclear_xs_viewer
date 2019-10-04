68166364b0374e8eb91012be7908d994617bceba
shimwell <mail@jshimwell.com>
2019-10-04 09:37:55 +0100

    replaced MeV with eV

--- src/App.js
+++ src/App.js
@@ -266,17 +266,17 @@ function PlotlyGraph(props) {
       
       if (mt_number == '301'){ 
           if (y_axis_title.indexOf('heating') == -1){
-            y_axis_title = y_axis_title+' heating (eV/reaction) '
+            y_axis_title = y_axis_title+'  heating (eV/reaction)  '
           }
       }
       else if (mt_number == '444'){
               if (y_axis_title.indexOf('heating') == -1){
-                y_axis_title = y_axis_title+' damage (MeV-barns) '
+                y_axis_title = y_axis_title+'  damage (eV-barns)  '
               }
       }
       else{
         if (y_axis_title.indexOf('cross section') == -1){
-          y_axis_title = y_axis_title + 'cross section (barns)'
+          y_axis_title = y_axis_title + '  cross section (barns)  '
         }
       }
         
