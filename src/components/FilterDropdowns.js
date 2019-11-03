d96e87cc568ed6ee836f4d4cdb97758df92660ae
Ahmed Eid <a.eid@yandex.com>
2019-11-03 18:04:58 +0200

    add clearable dropdown

--- src/components/FilterDropdowns.js
+++ src/components/FilterDropdowns.js
@@ -1,29 +1,29 @@
-import React from "react";
-import Select from "react-select";
+import React from "react"
+import Select from "react-select"
 
 const FilterDropdowns = ({ filter_data, event_handler }) => (
   <div>
     {filter_data.map((x, i) => {
-      const meta_data_dropdown_dict = [];
-      const list_of_dropdown_values = x["distinct_values"];
-      const field_values = x["field"][0];
+      const meta_data_dropdown_dict = []
+      const list_of_dropdown_values = x["distinct_values"]
+      const field_values = x["field"][0]
 
       meta_data_dropdown_dict.push({
         value: {
           field: field_values,
-          value: ""
+          value: "",
         },
-        label: ""
-      });
+        label: "",
+      })
 
       for (var j = 0; j < list_of_dropdown_values.length; j++) {
         meta_data_dropdown_dict.push({
           value: {
             field: field_values,
-            value: list_of_dropdown_values[j]
+            value: list_of_dropdown_values[j],
           },
-          label: list_of_dropdown_values[j]
-        });
+          label: list_of_dropdown_values[j],
+        })
       }
 
       return (
@@ -32,6 +32,7 @@ const FilterDropdowns = ({ filter_data, event_handler }) => (
             {field_values}
             <div className="haha">
               <Select
+                isClearable
                 id={"dropdown_" + field_values.replace(" ", "_")}
                 key={i}
                 options={meta_data_dropdown_dict}
@@ -43,9 +44,9 @@ const FilterDropdowns = ({ filter_data, event_handler }) => (
             </div>
           </label>
         </div>
-      );
+      )
     })}
   </div>
-);
+)
 
-export default FilterDropdowns;
+export default FilterDropdowns
