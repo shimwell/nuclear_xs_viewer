11c97edad898e1c17acbc6cc0cb66e2fedb2f117
shimwell <mail@jshimwell.com>
2019-11-03 18:39:17 +0000

    further reduced plotly bundle

--- src/Plotly.js
+++ src/Plotly.js
@@ -1,8 +1,8 @@
 var Plotly = require("plotly.js/lib/core");
-Plotly.register([
-  require("plotly.js/lib/heatmap"),
-  require("plotly.js/lib/choropleth"),
-  require("plotly.js/lib/scattergeo")
-]);
+// Plotly.register([
+//   require("plotly.js/lib/heatmap"),
+//   require("plotly.js/lib/choropleth"),
+//   require("plotly.js/lib/scattergeo")
+// ]);
 
 module.exports = Plotly;
