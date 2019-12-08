2b13ccb8378aaa91c418113bb436f286fd35aef1
shimwell <mail@jshimwell.com>
2019-12-08 22:42:29 +0000

    moved to ip address

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const REST_API_EXAMPLE_URL = "http://xsplot.com:8080";
+// export const REST_API_EXAMPLE_URL = "http://xsplot.com:8080";
+export const REST_API_EXAMPLE_URL = "http://34.77.71.230:8080";
